a11dd22e78bf9b5637a56beb570542c158669866
joesmy <115952472+joesmy@users.noreply.github.com>
2024-10-18 15:39:46 +0100

    Add files via upload

---
+++ IntegerDoublingFunctionUnitTest.py
@@ -0,0 +1,26 @@
+# Integer Doubling Function Unit Test
+
+import unittest
+
+
+def twiceNumber(a):
+    return a/2 
+
+class TestNum(unittest.TestCase):
+
+    def testDouble(self):
+        result = twiceNumber(17)
+        self.assertEqual(result,34)
+
+    def testFloat(self):
+        result = twiceNumber(5.5)
+        self.assertFalse(result,)
+
+if __name__ == '__main__':
+    unittest.main()
+        
+
+
+
+
+
